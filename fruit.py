43064ea4e9535a4824428eacb9ff0bf73045f404
ryan-kttam <ryantam0626@gmail.com>
2018-02-11 23:36:33 -0800

    organize the functions and comments

--- fruit.py
+++ fruit.py
@@ -2,50 +2,51 @@ import numpy as np
 import cv2
 import glob
 import os
+import random
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Dropout, Dense, Flatten
+from keras.models import Sequential, Model
+from keras import optimizers, applications
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
+# reading_images: a function that does the following steps:
+# 1. store the image pixels to fruit_img, and the label names to fruit_labels
+# 2. Read every image
+# 3. resize the images
+# 4. standardize the images
+# 5. return a list of image pixels and a list of labels
 def reading_images(fruit_img, fruit_labels, img_folder):
     for fruit_dir_path in glob.glob(img_folder):
         fruit_name = fruit_dir_path.split("\\")[-1]
-#        print(fruit_name)
         for image_path in glob.glob(os.path.join(fruit_dir_path, "*.jpg")):
             image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-#            print(image_path)
             image = cv2.resize(image, (100, 100))
             fruit_img.append((image / 255.).tolist())
             fruit_labels.append(fruit_name)
         for image_path in glob.glob(os.path.join(fruit_dir_path, "*.jpeg")):
             image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-#            print(image_path)
             image = cv2.resize(image, (100, 100))
             fruit_img.append((image / 255.).tolist())
             fruit_labels.append(fruit_name)
         for image_path in glob.glob(os.path.join(fruit_dir_path, "*.png")):
             image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-#            print(image_path)
             image = cv2.resize(image, (100, 100))
             fruit_img.append((image / 255.).tolist())
             fruit_labels.append(fruit_name)
     return fruit_img, fruit_labels
 
-
-# glob.glob("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/food/training/*")
-
+# creating an empty list for storing the images and labels
 fruit_images = []
 labels = []
 
 fruit_images, labels = reading_images(fruit_images, labels,
                                       "C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/new_fruit/fruits/*")
 
-len(fruit_images)
-110 + 115 + 108 + 120 + 100 + 99 + 100 + 108 + 101 + 100 + 120 + 117 + 116 + 100 + 101 + 100 + 115 + 100 + 100 + 119 + 100 + 100
-
-#import collections
-#collections.Counter(labels)
-
+# helper_label: A helper function that help me encode the labels to 0,1
 def helper_label(list_of_labels):
     from sklearn.preprocessing import OneHotEncoder, LabelEncoder
     encoder = LabelEncoder()
-    # turn the labels to 0 - 45
+    # turn the labels to 0 - 21
     encoder_int = encoder.fit_transform(list_of_labels)
     encoder_int = encoder_int.reshape(len(encoder_int), 1)
     one_hot_encoder = OneHotEncoder(sparse=False)
@@ -55,12 +56,9 @@ def helper_label(list_of_labels):
 encoded_labels = helper_label(labels)
 
 # generate random index numbers
-import random
-
 # randomize the index
 idx = random.sample(range(len(labels)), len(labels))
-
-# turn into array, to make the calculation faster?
+# Turn into array for later modification
 idx = np.array(idx)
 encoded_labels = np.array(encoded_labels)
 fruit_images = np.array(fruit_images)
@@ -68,7 +66,6 @@ fruit_images = np.array(fruit_images)
 train_idx = idx[:int(len(idx) * 0.8)]
 validation_idx = idx[int(len(idx) * 0.8):int(len(idx) * 0.95)]
 test_idx = idx[int(len(idx) * 0.95):]
-
 # apply those index to training, validation, and test set
 labels_train = encoded_labels[train_idx]
 image_train = fruit_images[train_idx]
@@ -76,47 +73,101 @@ labels_valid = encoded_labels[validation_idx]
 image_valid = fruit_images[validation_idx]
 labels_test = encoded_labels[test_idx]
 image_test = fruit_images[test_idx]
-# check the distribution of the labels
-sum(encoded_labels)
-sum(labels_train)
-sum(labels_valid)
-sum(labels_test)
-# frequency table, if needed
-# from collections import Counter
-# freq = Counter(labels)
 
 
 # -------------------------- B E N C H M A R K   M O D E L  ------------------------
-# run a benchmark model
-# basic benchmark model (in development)
-from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
-from keras.layers import Dropout, Dense, Flatten
-from keras.models import Sequential
-
+# constructing a benchmark model
+# 1. flatten the pixels
+# 2. add two hidden layers with 64 nodes, and ReLU as activation
+# 3. after each hidden layer, add a dropout layet with 0.2 probability
+# 4. add a final softmax layer with 22 nodes
 benchmark_model = Sequential()
 benchmark_model.add(Flatten(input_shape=image_train.shape[1:]))  # image_train.shape[1:] is (100,100,3)
-benchmark_model.add(Dense(256, activation='relu'))
-# benchmark_model.add(Dropout(0.3))
-benchmark_model.add(Dense(256, activation='relu'))
-benchmark_model.add(Dropout(0.3))
+benchmark_model.add(Dense(64, activation='relu'))
+benchmark_model.add(Dropout(0.2))
+benchmark_model.add(Dense(64, activation='relu'))
+benchmark_model.add(Dropout(0.2))
 benchmark_model.add(Dense(22, activation='softmax'))
 benchmark_model.summary()
 
 benchmark_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+checkpt = ModelCheckpoint("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/checkpt.h5", monitor='val_acc',
+                          save_best_only=True, save_weights_only=False, mode='auto', period=1)
+early_stop = EarlyStopping(monitor='val_acc', min_delta=0, patience=5, mode='auto')
 benchmark_model.fit(image_train, labels_train,
                     validation_data=(image_valid, labels_valid),
-                    batch_size=50, epochs=10)
+                    batch_size=50, epochs=20, callbacks=[checkpt, early_stop])
+benchmark_model.load_weights("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/checkpt.h5")
+prediction = benchmark_model.predict(image_valid)
+number_correct = 0
+for i in range(len(prediction)):
+    if np.where(prediction[i] == max(prediction[i])) == np.where(labels_valid[i] == 1):
+        number_correct += 1
+# the accuracy on the validation set is 5.97%
+print("For Benchmark model, The accuracy on validation set is {:.2f}%".format(number_correct / len(prediction) * 100))
 
 # -------------------------- B E N C H M A R K   M O D E L  E N D ------------------------
+#
+# optimizer_generator is a helper function that takes the name of the optimizer (rms or sgd) and learning rate as input,
+# then it generates an optimizer
+# optimizer: "rms" or "sgd"
+# learn_rate: default to 0.01
+def optimizer_generator(optimizer, learn_rate=0.01):
+    if optimizer not in ("rms", "sgd"):
+        return "please enter rms or sgd as optimizer"
+    if optimizer == "rms":
+        return optimizers.RMSprop(lr=learn_rate)  # or 0.001 etc
+    else:
+        return optimizers.SGD(lr=learn_rate)
+
+
+def fine_tuning(cnn_model, optimizer, learn_rate, model_type = ""):
+    # fine_tuning is a function that compile a provided CNN model using a provided optimizer name and learning rates.
+    # cnn_model: accept any type of cnn model
+    # optimizer: "rms" or "sgd"
+    # learn_rate: 0.1, 0.01, or 0.001
+    # model_type: "scratch" or "pre_trained"
+
+
+    if model_type not in ("scratch", "pre_trained") :
+        return "please specify which model you are training. (scratch or pre_trained)"
+
+    model_optimizer = optimizer_generator(optimizer, learn_rate)
+    cnn_model.compile(optimizer=model_optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    checkpt = ModelCheckpoint("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/checkpt.h5",
+                              monitor='val_acc', save_best_only=True,
+                              save_weights_only=False, mode='auto', period=1)
+    early_stop = EarlyStopping(monitor='val_acc', min_delta=0, patience=5, mode='auto')
+
+    cnn_model.fit(image_train, labels_train,
+                  validation_data=(image_valid, labels_valid),
+                  batch_size=100, epochs=50, callbacks=[checkpt, early_stop])
+    cnn_model.load_weights("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/checkpt.h5")
+
+    if model_type == "scratch":
+        valid_pred_class = cnn_model.predict_classes(image_valid)
+        number_correct = 0
+        for i in range(len(labels_valid)):
+            if valid_pred_class[i] == np.where(labels_valid == 1)[1][i]:
+                number_correct += 1
+        print("For {} with learning rate {}, The accuracy on test set is {:.2f}%".format(optimizer, learn_rate, number_correct / len(valid_pred_class) * 100))
+
+    elif model_type == "pre_trained":
+        prediction = cnn_model.predict(image_valid)
+        number_correct = 0
+        for i in range(len(prediction)):
+            if np.where(prediction[i] == max(prediction[i])) == np.where(labels_valid[i] == 1):
+                number_correct += 1
+        print("For {} with learning rate {}, The accuracy on test set is {:.2f}%".format(optimizer, learn_rate,
+                                                                                         number_correct / len(prediction) * 100))
+    return cnn_model
+
+
 # ----------------M O D E L   F R O M   S C R A T C H ---------------------------
 # train the model
-from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
-from keras.layers import Dropout, Dense, Flatten
-from keras.models import Sequential
-from keras import optimizers
-from keras.callbacks import ModelCheckpoint, EarlyStopping
 
 def cnn_from_scratch():
+    # create a CNN model from scratch
     model = Sequential()
     model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu', input_shape=(100, 100, 3)))
     model.add(MaxPooling2D(pool_size=2))
@@ -130,65 +181,27 @@ def cnn_from_scratch():
     model.summary()
     return model
 
-# optimizer_generator generate an optimizer
-# optimizer: "rms" or "sgd"
-# learn_rate: default to 0.01
-def optimizer_generator(optimizer, learn_rate=0.01):
-    if optimizer not in ("rms", "sgd"):
-        return "please enter rms or sgd as optimizer"
-    if optimizer == "rms":
-        return optimizers.RMSprop(lr=learn_rate)  # or 0.001 etc
-    else:
-        return optimizers.SGD(lr=learn_rate)
 
-def fine_tuning(cnn_model, optimizer, learn_rate, model_type = ""):
-    if model_type == "":
-        return "please specify which model you are training."
-    model_optimizer = optimizer_generator(optimizer, learn_rate)
-    cnn_model.compile(optimizer=model_optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-    checkpt = ModelCheckpoint("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/checkpt.h5", monitor='val_acc',
-                              save_best_only=True, save_weights_only=False,
-                              mode='auto', period=1)
-    early_stop = EarlyStopping(monitor='val_acc', min_delta=0, patience=5, mode='auto')
+m = fine_tuning(cnn_from_scratch(), optimizer="rms",
+                learn_rate=0.01, model_type="scratch")
 
-    cnn_model.fit(image_train, labels_train,
-                  validation_data=(image_valid, labels_valid),
-                  batch_size=100, epochs=50, callbacks=[checkpt, early_stop])
-    cnn_model.load_weights("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/checkpt.h5")
-    if model_type == "scratch":
-        valid_pred_class = cnn_model.predict_classes(image_valid)
-        number_correct = 0
-        for i in range(len(labels_valid)):
-            if valid_pred_class[i] == np.where(labels_valid == 1)[1][i]:
-                number_correct += 1
-        print("For {} with learning rate {}, The accuracy on test set is {:.2f}%".format(optimizer, learn_rate, number_correct / len(valid_pred_class) * 100))
-
-    elif model_type == "pre_trained":
-        prediction = transfered_model.predict(image_valid)
-        number_correct = 0
-        for i in range(len(prediction)):
-            if np.where(prediction[i] == max(prediction[i])) == np.where(labels_valid[i] == 1):
-                number_correct += 1
-        print("For {} with learning rate {}, The accuracy on test set is {:.2f}%".format(optimizer, learn_rate,
-                                                                                         number_correct / len(prediction) * 100))
-
-fine_tuning(cnn_from_scratch(), "rms", 0.1)
-
-m = cnn_from_scratch()
 m.load_weights("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/checkpt.h5")
-valid_pred_class = m.predict_classes(image_valid)
-number_correct = 0
+
+# make prediction on validation set to get the validation accuracy
+scratch_prediction = m.predict_classes(image_valid)
+scratch_prediction_correct = 0
 for i in range(len(labels_valid)):
-    if valid_pred_class[i] == np.where(labels_valid == 1)[1][i]:
-        number_correct += 1
-        # ----------------M O D E L   F R O M   S C R A T C H  E N D ---------------------------
+    if scratch_prediction[i] == np.where(labels_valid == 1)[1][i]:
+        scratch_prediction_correct += 1
 
 # -------------------------- T R A N S F E R   L E A R N I N G ----------------------------
-from keras import applications
-from keras.models import Model
-from keras.callbacks import ModelCheckpoint, EarlyStopping
+
 
 def pre_trained(trainable): # trainable is a boolean
+    # create a pre-trained model (VGG16)
+    # trainable: TRUE/ FALSE
+    #   TRUE means the last five layers are trainable
+    #   FALSE means non of the layers are trainable
     model = applications.VGG16(weights="imagenet", include_top=False, input_shape=(100, 100, 3))
     if not trainable:
         for layer in model.layers[:]:
@@ -196,73 +209,52 @@ def pre_trained(trainable): # trainable is a boolean
     else:
         for layer in model.layers[:-5]:
             layer.trainable = False
-
     x = model.output
     x = Flatten()(x)
-    x = Dense(1024, activation='relu')(x)  # or 512
+    x = Dense(1024, activation='relu')(x)
     x = Dropout(0.2)(x)
     x = Dense(22, activation='softmax')(x)
     transfered_model = Model(inputs=model.input, output=x)
     transfered_model.summary()
     return transfered_model
 
-#aa = optimizers.RMSprop(lr=0.001)
 
-fine_tuning(pre_trained(False), "rms", 0.001, "pre_trained")
+transfered_model = fine_tuning(pre_trained(False), "rms", 0.001, "pre_trained")
 
-transfered_model.compile(optimizer=aa, loss='categorical_crossentropy', metrics=['accuracy'])
-
-checkpt = ModelCheckpoint("vgg16_1.h5", monitor='val_acc',
-                          save_best_only=True, save_weights_only=False,
-                          mode='auto', period=1)
-early_stop = EarlyStopping(monitor='val_acc', min_delta=0, patience=5, mode='auto')
-
-transfered_model.fit(image_train, labels_train,
-                     validation_data=(image_valid, labels_valid),
-                     batch_size=100, epochs=20, callbacks=[checkpt, early_stop])
-
-pp = transfered_model.predict(image_valid)
-number_correct = 0
-for i in range(len(pp)):
-    if np.where(pp[i] == max(pp[i])) == np.where ( labels_valid[i] == 1):
-        number_correct += 1
-
-print("For {} with learning rate {}, The accuracy on test set is {:.2f}%".format(optimizer, learn_rate, number_correct / len(valid_pred_class) * 100))
-np.where ( labels_valid[250] == 1) == np.where(pp[250] == max(pp[250]) )
-
-# ------------------------end transfer learning --------------------------
+# make prediction on validation set to get the validation accuracy
+pred = transfered_model.predict(image_valid)
+pred_correct = 0
+for i in range(len(pred)):
+    if np.where(pred[i] == max(pred[i])) == np.where(labels_valid[i] == 1):
+        pred_correct += 1
+print("For {} with learning rate {}, The accuracy on test set is {:.2f}%".format(optimizer, learn_rate, pred_correct / len(labels_valid) * 100))
 
 
-# test the trained model on test set
+# Apply the best model to test set
 # test_result: real label, number of position where 1 occurs (from 0-40)
 # np.where(labels_test == 1): pred label, number of position where 1 occurs (from 0-40)
-test_result = model.predict_classes(image_test)
+test_result = transfered_model.predict_classes(image_test)
 number_correct = 0
 for i in range(len(test_result)):
     if test_result[i] == np.where(labels_test == 1)[1][i]:
         number_correct += 1
 print("The accuracy on test set is {:.2f}%".format(number_correct / len(test_result) * 100))
 
-# test on real life picture
-test_path = glob.glob("C:/Users/tam9/Desktop/Python/fruit_image/test/*")
-# test_path = "C:/Users/tam9/Desktop/Python/fruit_image/test\lemon.jpg)"
-test_img = cv2.imread(test_path[8], cv2.IMREAD_COLOR)
-test_img = cv2.resize(test_img, (50, 50))
-# test_img = test_img / 255.
-test_img = test_img.reshape(1, 50, 50, 3)
-model.predict_classes(test_img)
 
-"C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/new_fruit/fruits/*"
-
-a = glob.glob(os.path.join("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/new_fruit/fruits/*", "*.jpg"))
-glob.glob(os.path.join("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/new_fruit/fruits/*", "*.jpeg"))
-a = glob.glob(os.path.join("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/new_fruit/fruits/*", "*.png"))
-
-a[1]
-
-b = cv2.imread(a[1], cv2.IMREAD_COLOR)
-c = cv2.resize(b, (50, 50))
-len(c)
-
-glob.glob(os.path.join(fruit_dir_path, "*.jpg"))
-a
+# Apply the model model to self-taken images
+real_life_img = []
+real_life_labels = []
+real_life_img, real_life_labels = reading_images(real_life_img, real_life_labels,
+                                      "C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/new_fruit/real_life_test/*")
+t_model = pre_trained(False)
+t_model.load_weights("C:/Users/Ryan/Desktop/Udacity Machine Learning/capstone/checkpt.h5")
+prediction = t_model.predict(image_test)
+number_correct = 0
+for i in range(len(prediction)):
+    if np.where(prediction[i] == max(prediction[i])) == np.where(labels_test[i] == 1):
+        number_correct += 1
+    else:  # show which ones are predicted wrong
+        plt.imshow(image_test[i])
+        print("For position: {}, Prediction: {}, correct answer is: {}".format(i,
+                                                                               np.where(prediction[i] == max(prediction[i])),
+                                                                               np.where(labels_test[i] == 1)))
